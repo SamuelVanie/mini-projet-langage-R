a53756380039f78010d5d2da086167caca277039
SamuelVanie <vaniebi59@gmail.com>
2022-06-18 12:25:33 -0400

    feat: Réponse à la première question
    Réponse à la premiere question concernant la fonction
    toName(mail) qui devait retourner un dataframe contenant
    le nom, le prénom et le mail de l'étudiant.

---
+++ mailManipulation.R
@@ -0,0 +1,22 @@
+# Programme de manipulation du mail des étudiants de l'INPHB
+# de Yamoussoukro.
+# Ecrit par Samuel Michaël Vanié
+
+
+# Chargement de la library stringr qui contient la fonction str_extract_all
+# qui permet de récupérer des groupes qui ont été matchés à partir
+# d'un pattern
+library("stringr")
+
+#'@title toName
+#'@description fonction qui à partir récupère le nom, le prénom
+# et l'adresse mail d'un étudiant à partir de son mail
+#'@param mail : C'est le mail institutionnel à traiter
+#'@return identifiants, dataframe qui contient le nom, le prénom
+# et l'adresse mail récupérés.
+toName <- function(mail) {
+  liste <- str_extract_all(mail, "\\w+[^\\d\\.@]")
+  donnee <- as.vector(liste)
+  identifiants <- data.frame(nom = donnee[[1]][[1]], prenom = donnee[[1]][[2]], email = mail)
+  return(identifiants)
+}
